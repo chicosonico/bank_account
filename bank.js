a48f270867bed9e76c695807dc07302204448622
pocotacto <studio@pocotacto.info>
2020-02-10 18:07:57 +0100

    First Commit
    Classes Account and Client created.

---
+++ bank.js
@@ -0,0 +1,73 @@
+class BankAccount {
+    constructor(accountID, client, balance, movements) {
+      this._accountID = accountID;
+      this._client = client;
+      this._balance = balance;
+      this._movements = movements;
+    }
+
+    get getAccountID() {
+        return this._accountID;
+    }
+    get getClient() {
+        return this._client;
+    }
+    get getBalance() {
+        return this._balance;
+    }
+    get getMovements() {
+        return this._movements;
+    }
+
+   
+
+    set setClient(editClient) {
+        this._client = editClient;
+      }
+
+    set setBalance(editBalance) {
+        this._balance = editBalance;
+      }
+
+    set setMovements(editMovements) {
+        this._movements = editMovements;
+      } 
+
+ } 
+
+
+
+ class Client{
+
+    constructor(clientID, clientName, clientAccounts) {
+        this._clientID = clientID;
+        this._clientName = clientName;
+        this._clientAccounts = clientAccounts;
+      }
+
+    get getClientID() {
+        return this._clientID;
+    }
+    get getClientName() {
+        return this._clientName;
+    }
+    get getClientAccounts() {
+        return this._clientAccounts;
+    }
+  
+
+    set setClientName(editClientName) {
+        this._clientName = editClientName;
+      }
+
+    set setClientAccounts(editClientAccounts) {
+        this._clientAccounts = editClientAccounts;
+      }
+
+  
+
+
+
+
+ }
+
